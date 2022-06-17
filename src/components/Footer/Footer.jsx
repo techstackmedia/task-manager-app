3e0a3ab43bacef77e280532ae5b90a63c79372a2
techstackmedia <osagienoah04@gmail.com>
2022-06-17 22:19:31 +0100

    add footer delete icon

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,18 @@
+import { MdDelete } from 'react-icons/md';
 import footer from './Footer.module.css';
 
 const Footer = () => {
   return (
     <footer className={footer.footer}>
       <div>
-        <div>Cancel</div>
         <div>
-          <button>Save</button>
+          <MdDelete />
+        </div>
+        <div>
+          <div>Cancel</div>
+          <div>
+            <button>Save</button>
+          </div>
         </div>
       </div>
     </footer>
